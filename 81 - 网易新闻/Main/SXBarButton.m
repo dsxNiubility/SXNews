dc7eb7d69411fc7a51a5eee86a0fa5908320b990
董尚先 <dantesx2012@gmail.com>
2015-07-20 18:13:24 +0800

    修改底部tabbar图标挤压失真

--- 81 - 网易新闻/Main/SXBarButton.m
+++ 81 - 网易新闻/Main/SXBarButton.m
@@ -24,10 +24,12 @@
     self.imageView.width = 25;
     self.imageView.height = 25;
     self.imageView.x = (self.width - self.imageView.width)/2.0;
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
     self.titleLabel.x = self.imageView.x - (self.titleLabel.width - self.imageView.width)/2.0;
     self.titleLabel.y = CGRectGetMaxY(self.imageView.frame) + 2;
     
     self.titleLabel.font = [UIFont systemFontOfSize:10];
+    self.titleLabel.shadowColor = [UIColor clearColor];
     
 //    self.backgroundColor = [UIColor redColor];
     
