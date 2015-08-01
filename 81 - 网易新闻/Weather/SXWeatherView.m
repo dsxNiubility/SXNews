0b5d98b2e23e17c78e994ecd2eb61b5b42582b64
董尚先 <dantesx2012@gmail.com>
2015-08-01 12:29:52 +0800

    建立weatherView

---
+++ 81 - 网易新闻/Weather/SXWeatherView.m
@@ -0,0 +1,21 @@
+//
+//  SXWeatherView.m
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 15/8/1.
+//  Copyright (c) 2015年 ShangxianDante. All rights reserved.
+//
+
+#import "SXWeatherView.h"
+
+@implementation SXWeatherView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
