48fa5bde7a79bb49a182db1103fffb0fb7f4b864
董尚先 <dantesx2012@gmail.com>
2015-08-08 17:50:42 +0800

    添加天气详情页控制器

---
+++ 81 - 网易新闻/Weather/SXWeatherDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  SXWeatherDetailVC.h
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 15/8/8.
+//  Copyright (c) 2015年 ShangxianDante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXWeatherDetailVC : UIViewController
+
+@end
---
+++ 81 - 网易新闻/Weather/SXWeatherDetailVC.m
@@ -0,0 +1,37 @@
+//
+//  SXWeatherDetailVC.m
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 15/8/8.
+//  Copyright (c) 2015年 ShangxianDante. All rights reserved.
+//
+
+#import "SXWeatherDetailVC.h"
+
+@interface SXWeatherDetailVC ()
+
+@end
+
+@implementation SXWeatherDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
