e7c7b4ae7f66d96c49ad4dbced7a1b955ce88a18
董尚先 <dantesx2012@gmail.com>
2016-01-26 19:51:53 +0800

    做了一些搜索栏相关处理

--- 81 - 网易新闻/Search/SXSearchPage.m
+++ 81 - 网易新闻/Search/SXSearchPage.m
@@ -7,8 +7,9 @@
 //
 
 #import "SXSearchPage.h"
+#import "UIView+Frame.h"
 
-@interface SXSearchPage ()<UITableViewDataSource,UITableViewDelegate>
+@interface SXSearchPage ()<UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -21,6 +22,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.searchBar.height = 20;
     // Do any additional setup after loading the view.
     
     
@@ -51,19 +54,18 @@
     return [UITableViewCell new];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar;
+{
+    NSLog(@"%@",searchBar.text);
+    if (self.beginView.superview) {
+        [self.beginView removeFromSuperview];
+    }
+    [searchBar resignFirstResponder];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
 }
-*/
 
 @end
