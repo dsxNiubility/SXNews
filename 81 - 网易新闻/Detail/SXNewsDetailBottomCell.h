a7bd5e3cede7a3487b5bb81bed7acfe27ef989d3
董尚先 <dantesx2012@gmail.com>
2016-01-29 17:31:54 +0800

    把新闻详情页下面的几种cell的样式都已经搭建完成

---
+++ 81 - 网易新闻/Detail/SXNewsDetailBottomCell.h
@@ -0,0 +1,13 @@
+//
+//  SXNewsDetailBottomCell.h
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 16/1/29.
+//  Copyright © 2016年 ShangxianDante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXNewsDetailBottomCell : UITableViewCell
+
+@end
---
+++ 81 - 网易新闻/Detail/SXNewsDetailBottomCell.m
@@ -0,0 +1,23 @@
+//
+//  SXNewsDetailBottomCell.m
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 16/1/29.
+//  Copyright © 2016年 ShangxianDante. All rights reserved.
+//
+
+#import "SXNewsDetailBottomCell.h"
+
+@implementation SXNewsDetailBottomCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
