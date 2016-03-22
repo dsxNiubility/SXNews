22775649a42953758175e0cfc0ba1e9584435ff8
董尚先 <dantesx2012@gmail.com>
2016-03-22 13:47:01 +0800

    评价页ViewModel初步完成 ，等待替换和调用

--- 81 - 网易新闻/Reply/SXReplyViewModel.h
+++ 81 - 网易新闻/Reply/SXReplyViewModel.h
@@ -7,7 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SXReplyEntity.h"
+#import "SXNewsEntity.h"
+
+typedef NS_ENUM(NSUInteger, SXReplyPageFrom) {
+    SXReplyPageFromNewsDetail = 0,
+    SXReplyPageFromPhotoset = 1,
+};
 
 @interface SXReplyViewModel : NSObject
+/**
+ *  评论列表
+ */
+@property(nonatomic,strong) NSMutableArray *replyModels;
+/**
+ *  普通评论列表
+ */
+@property(nonatomic,strong) NSMutableArray *replyNormalModels;
+/**
+ *  获取热门回复命令
+ */
+@property(nonatomic, strong) RACCommand *fetchHotReplyCommand;
+/**
+ *  获取普通回复命令
+ */
+@property(nonatomic, strong) RACCommand *fetchNormalReplyCommand;
+/**
+ *  新闻概览模型
+ */
+@property(nonatomic,strong) SXNewsEntity *newsModel;
+/**
+ *  评价页的来源
+ */
+@property(nonatomic,assign)SXReplyPageFrom source;
 
 @end
