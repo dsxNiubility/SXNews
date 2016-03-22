2398e8749f6abd2705437518ced69f2149aad6a3
董尚先 <dantesx2012@gmail.com>
2016-03-22 11:34:27 +0800

    代码结构调整，回复单里一个模块 便于之后复用

---
+++ 81 - 网易新闻/Reply/SXReplyViewModel.h
@@ -0,0 +1,13 @@
+//
+//  SXReplyViewModel.h
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 16/3/22.
+//  Copyright © 2016年 ShangxianDante. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SXReplyViewModel : NSObject
+
+@end
---
+++ 81 - 网易新闻/Reply/SXReplyViewModel.m
@@ -0,0 +1,13 @@
+//
+//  SXReplyViewModel.m
+//  81 - 网易新闻
+//
+//  Created by dongshangxian on 16/3/22.
+//  Copyright © 2016年 ShangxianDante. All rights reserved.
+//
+
+#import "SXReplyViewModel.h"
+
+@implementation SXReplyViewModel
+
+@end
